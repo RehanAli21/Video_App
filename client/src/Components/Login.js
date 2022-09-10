a22ba926bf2b5224622ff1b65c7e02a1ec38965c
Rehan Ali Shah <rehan.shah70@yahoo.com>
2022-09-10 12:13:40 +0500

    login is working

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 
 const Login = () => {
+	const [email, setEmail] = useState('')
+	const [pass, setPass] = useState('')
+
+	const login = async () => {
+		if (email && email !== '' && pass && pass !== '') {
+			try {
+				const res = await axios.get(`http://localhost:5000/api/user/login/${email}/${pass}`)
+				console.log(res.data.msg)
+			} catch (err) {
+				alert(err)
+			}
+		}
+	}
+
 	return (
 		<div>
 			<h1>Login</h1>
-			<input type='email' placeholder='Email' />
-			<input type='password' placeholder='Password' />
-			<button>Login</button>
+			<input onChange={e => setEmail(e.target.value)} type='email' placeholder='Email' />
+			<input onChange={e => setPass(e.target.value)} type='password' placeholder='Password' />
+			<button onClick={login}>Login</button>
 		</div>
 	)
 }
